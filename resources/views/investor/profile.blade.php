9b22af004ba90efef05010427363814d21a9c403
permadimupe <35791368+permadimupe@users.noreply.github.com>
2018-01-26 10:19:35 +0700

    r tetett

--- resources/views/investor/profile.blade.php
+++ resources/views/investor/profile.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
 {{--  Untuk menampilkan dan mengedit profil investor  --}}
+<section class="probootstrap_section" id="section-feature-testimonial">
+    <div class="container">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-12 text-center mb-5 probootstrap-animate fadeInUp probootstrap-animated">
+          <h2 class="display-4 border-bottom probootstrap-section-heading">Profil Pengusaha</h2>
+          <blockquote class="">
+            <p class="lead mb-4"><em>Data Pengusaha</em></p>
+            <p class="probootstrap-author">
+              <a href="https://probootstrap.com/" target="_blank">
+                <img src="assets/images/person_1.jpg" alt="Free Template by ProBootstrap.com" class="rounded-circle">
+                <span class="probootstrap-name">Nama Pengusaha</span>
+                <span class="probootstrap-title">Foto Pengusaha</span>
+              </a>
+            </p>
+          </blockquote>
+
+        </div>
+      </div>
+      
+    </div>
+  </section>
+
 @endsection
--- resources/views/pengusaha/profile.blade.php
+++ resources/views/pengusaha/profile.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
 {{--  Untuk menampilkan dan edit profil  --}}
+
+<section class="probootstrap_section" id="section-feature-testimonial">
+    <div class="container">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-12 text-center mb-5 probootstrap-animate fadeInUp probootstrap-animated">
+          <h2 class="display-4 border-bottom probootstrap-section-heading">Profil Investor</h2>
+          <blockquote class="">
+            <p class="lead mb-4"><em>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</em></p>
+            <p class="probootstrap-author">
+              <a href="https://probootstrap.com/" target="_blank">
+                <img src="assets/images/person_1.jpg" alt="Free Template by ProBootstrap.com" class="rounded-circle">
+                <span class="probootstrap-name">Nama Investor</span>
+                <span class="probootstrap-title">Chief Executive Officer</span>
+              </a>
+            </p>
+          </blockquote>
+
+        </div>
+      </div>
+      
+    </div>
+  </section>
+
 @endsection
